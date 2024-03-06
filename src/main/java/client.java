11ac27c17bd9c5d2b48811499a68a3368d909db0
Arthur71130 <ademacedo.71@gmail.com>
2024-03-06 22:22:01 +0100

    Java Class client

---
+++ src/main/java/client.java
@@ -0,0 +1,15 @@
+public class client {
+    private String prenom;
+    private String nom;
+    private String mail;
+    private String nomUtilisateur;
+    private String mdp;
+
+    public client(String prenom,String nom,String mail,String nomUtilisateur,String mdp){
+        this.prenom = prenom;
+        this.nom = nom;
+        this.mail = mail;
+        this.nomUtilisateur = nomUtilisateur;
+        this.mdp = mdp;
+    }
+}
