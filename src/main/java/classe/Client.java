059ad706d32359a6918630d8c4b179a1dad70dbf
Arthur71130 <ademacedo.71@gmail.com>
2024-03-07 09:10:50 +0100

    Class Client Version 2

--- src/main/java/classe/Client.java
+++ src/main/java/classe/Client.java
@@ -1,15 +1,15 @@
-public class client {
+package classe;
+
+public class Client {
     private String prenom;
     private String nom;
     private String mail;
-    private String nomUtilisateur;
     private String mdp;
 
-    public client(String prenom,String nom,String mail,String nomUtilisateur,String mdp){
+    public Client(String prenom,String nom,String mail,String mdp){
         this.prenom = prenom;
         this.nom = nom;
         this.mail = mail;
-        this.nomUtilisateur = nomUtilisateur;
         this.mdp = mdp;
     }
 }
