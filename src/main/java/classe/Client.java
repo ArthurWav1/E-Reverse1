0ed620fdd41a603cb03596e2830b93f155f78ac5
ArthurWav <arthur.wavreille@etu.mines-ales.fr>
2024-03-21 00:55:28 +0100

    Précision classe Client Ajout lien entre la page connexion et la page d'inscription

--- src/main/java/classe/Client.java
+++ src/main/java/classe/Client.java
@@ -1,15 +1,30 @@
 package classe;
 
 public class Client {
+    private int id;
     private String prenom;
     private String nom;
     private String mail;
-    private String mdp;
+    private String adresse;
 
-    public Client(String prenom,String nom,String mail,String mdp){
+    public Client(String prenom,String nom,String mail,String adresse){
         this.prenom = prenom;
         this.nom = nom;
         this.mail = mail;
-        this.mdp = mdp;
+        this.adresse = adresse;
+    }
+
+    //Méthodes d'accès
+    public String getNom(){
+        return this.nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public String getAdresse() {
+        return adresse;
     }
 }
