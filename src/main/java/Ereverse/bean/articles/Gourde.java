397c1696ca3096a01b94710da8a635146a3048ca
PNLKRL <prunelle.ntsame-mezui@etu.mines-ales.fr>
2024-03-21 11:23:45 +0100

    Implementation des articles

--- src/main/java/Ereverse/bean/articles/Gourde.java
+++ src/main/java/Ereverse/bean/articles/Gourde.java
@@ -9,18 +9,17 @@ public class Gourde extends Article {
     private String couleur;
     private String taille;
     private String gamme;
-    private String nom_Article;
 
     public Gourde(String type_Article,String nom_gourde,String gamme, String taille, String couleur, int prix) {
         super(nom_gourde, type_Article, prix);
         this.couleur=couleur;
         this.gamme=gamme;
         this.taille=taille;
-        nom_Article=type_Article;
 
     }
 
-    public Gourde(int id_gourde,int reference,String type_Article, String nom_gourde,String gamme, String taille, String couleur, int prix) {
+    public Gourde(int id_gourde,int reference,String type_Article, String nom_gourde,String gamme,
+                  String taille, String couleur, int prix) {
         super(id_gourde, reference, nom_gourde, type_Article,prix);
     }
 
--- src/main/java/Ereverse/bean/articles/Pastille.java
+++ src/main/java/Ereverse/bean/articles/Pastille.java
@@ -1,7 +1,8 @@
 package Ereverse.bean.articles;
 
 /**
- *Une pastille est un article complement permettant de donner du goût à l'eau (the, cafe, etc); elle est vendue en option
+ *Une pastille est un article complement permettant de donner du goût à l'eau (the, cafe, etc);
+ *  elle est vendue en option
  * @author prune
  */
 public class Pastille extends Article {
@@ -11,12 +12,10 @@ public class Pastille extends Article {
     }
 
     private String saveur;
-    private String nom_Article;
 
     public Pastille(String type_Article, String saveur, String nom_pastille,int prix) {
         super(nom_pastille, type_Article, prix);
         this.saveur=saveur;
-        nom_Article=type_Article;
     }
 
     public Pastille(int id_pastille,int reference,String type_Article, String nom_pastille, int prix) {
