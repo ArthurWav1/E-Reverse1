2cc8de815b6b6fcac363b9f1cc148902ae183c54
ArthurWav <arthur.wavreille@etu.mines-ales.fr>
2024-03-21 11:56:13 +0100

    Suppression d'imports inutiles

--- src/main/java/servlet/Filtre/FiltreAuthentification.java
+++ src/main/java/servlet/Filtre/FiltreAuthentification.java
@@ -4,9 +4,6 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.platform.engine.FilterResult;
 
 import java.io.IOException;
 import java.util.List;
