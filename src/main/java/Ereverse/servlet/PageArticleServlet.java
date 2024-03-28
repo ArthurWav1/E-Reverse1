33aebf706854a4a11f63633e8458b223f4ade158
arthurdsfr <arthur.desfrancais@gmail.com>
2024-03-28 10:01:15 +0100

    ArticleDAO + PageArticleServlet

--- src/main/java/Ereverse/servlet/PageArticleServlet.java
+++ src/main/java/Ereverse/servlet/PageArticleServlet.java
@@ -1,4 +1,6 @@
 package Ereverse.servlet;
+import Ereverse.bean.articles.Article;
+import Ereverse.dao.ArticleDAO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,8 +12,21 @@ import java.io.IOException;
 @WebServlet({"/Article"})
 public class PageArticleServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    getServletContext().getRequestDispatcher("/jsp/PageArticle.jsp").forward(req,resp);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Récupérer l'identifiant de l'article à partir des paramètres de la requête
+        int ref_article =  Integer.parseInt(request.getParameter("ref_article"));
+
+        // Utiliser cet identifiant pour charger les informations de l'article depuis la base de données
+        // (vous pouvez utiliser votre DAO pour cela)
+        Article article = ArticleDAO.findById(ref_article);
+
+        // Mettre l'objet Article en tant qu'attribut de la requête pour l'affichage dans la JSP
+        request.setAttribute("Article", article);
+
+        // Rediriger vers la page JSP de l'article avec les informations chargées
+        request.getRequestDispatcher("/pageArticle.jsp").forward(request, response);
+
+        getServletContext().getRequestDispatcher("/jsp/PageArticle.jsp").forward(request,response);
     }
 }
 
