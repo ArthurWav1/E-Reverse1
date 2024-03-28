20e30981623baaf989bd2b235ca1a702d49deb68
Arthur71130 <ademacedo.71@gmail.com>
2024-03-28 09:28:56 +0100

    modification

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -1,7 +1,6 @@
 -- Création de la base de données
 CREATE DATABASE BDDReverse ;
 CREATE USER Admin WITH PASSWORD 'Reverse';
-CREATE SCHEMA public;
 GRANT ALL ON ALL TABLES IN SCHEMA public TO Admin;
 grant all privileges on all sequences in schema public to Admin;
 
