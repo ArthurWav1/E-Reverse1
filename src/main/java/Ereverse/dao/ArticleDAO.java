fd2471b59459c2fbab04694fd1197ca0c742095b
arthurdsfr <arthur.desfrancais@gmail.com>
2024-05-09 15:31:23 +0200

    Méthode liste gourde

--- src/main/java/Ereverse/dao/ArticleDAO.java
+++ src/main/java/Ereverse/dao/ArticleDAO.java
@@ -14,6 +14,7 @@ public class ArticleDAO {
 
     /**
      * Méthode pour obtenir un article à partir de sa référence
+     *
      * @param ref_article : référence de l'article
      * @return Article dont la référence est ref_article (null s'il n'existe pas)
      */
@@ -63,182 +64,216 @@ public class ArticleDAO {
 
     /**
      * Méthode permettant de renvoyer la liste des gourdes d'une certaine gamme
+     *
      * @param id_gamme : id de la gamme voulue
      * @return liste des gourdes de la gamme voulue (null si la liste est vide)
      */
-    public static ArrayList<Gourde> ListerGourde(int id_gamme){
-            Connection connection = null;
-            PreparedStatement statement = null;
-            ResultSet resultSet = null;
-            ArrayList<Gourde> ListeGourde = new ArrayList<>();
+    public static ArrayList<Gourde> ListerGourde(int id_gamme) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        ArrayList<Gourde> ListeGourde = new ArrayList<>();
 
+        try {
+            // Obtenir la connexion à la base de données
+            connection = ServiceConnexionBDD.getConnection();
+
+            // Requête SQL pour sélectionner tous les articles et les éléments
+            String query = "SELECT article.*, gamme.*,couleur.libelle AS libelle_couleur " +
+                    "FROM article " +
+                    "JOIN gamme ON article.id_gamme = gamme.id " +
+                    "JOIN couleur ON article.id_couleur = couleur.id " +
+                    "WHERE gamme.id = ?";
+            statement = connection.prepareStatement(query);
+            statement.setInt(1, id_gamme);
+            // Exécuter la requête
+            resultSet = statement.executeQuery();
+
+            // Parcourir les résultats et créer des objets Article pour chaque ligne
+            while (resultSet.next()) {
+                Gourde gourde = new Gourde(
+                        resultSet.getString("reference"),
+                        resultSet.getString("gamme"),
+                        resultSet.getInt("prix"),
+                        resultSet.getString("description"),
+                        resultSet.getString("libelle_couleur"),
+                        resultSet.getInt("volume")
+                        // Ajoutez d'autres attributs de l'article ici si nécessaire
+                );
+                ListeGourde.add(gourde);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Fermer les ressources
             try {
-                // Obtenir la connexion à la base de données
-                connection = ServiceConnexionBDD.getConnection();
-
-                // Requête SQL pour sélectionner tous les articles et les éléments
-                String query ="SELECT article.*, gamme.*,couleur.libelle AS libelle_couleur " +
-                              "FROM article " +
-                              "JOIN gamme ON article.id_gamme = gamme.id " +
-                              "JOIN couleur ON article.id_couleur = couleur.id "+
-                              "WHERE gamme.id = ?";
-                statement = connection.prepareStatement(query);
-                statement.setInt(1, id_gamme);
-                // Exécuter la requête
-                resultSet = statement.executeQuery();
-
-                // Parcourir les résultats et créer des objets Article pour chaque ligne
-                while (resultSet.next()) {
-                    Gourde gourde = new Gourde(
-                            resultSet.getString("reference"),
-                            resultSet.getString("gamme"),
-                            resultSet.getInt("prix"),
-                            resultSet.getString("description"),
-                            resultSet.getString("libelle_couleur"),
-                            resultSet.getInt("volume")
-                            // Ajoutez d'autres attributs de l'article ici si nécessaire
-                    );
-                    ListeGourde.add(gourde);
-                }
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
             } catch (SQLException e) {
                 e.printStackTrace();
-            } finally {
-                // Fermer les ressources
-                try {
-                    if (resultSet != null) resultSet.close();
-                    if (statement != null) statement.close();
-                    if (connection != null) connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
             }
-
-            return ListeGourde;
         }
+
+        return ListeGourde;
+    }
 
 
     /**
      * Méthode permettant de renvoyer tous les articles d'un certain type
      * (gourde/module/pastille/accessoire)
+     *
      * @param libelle_type : nom du type
      * @return la liste des articles du type demandé (null si la liste est vide)
      */
-    public static ArrayList<Article> ListerArticleType(String libelle_type){
-            Connection connection = null;
-            PreparedStatement statement = null;
-            ResultSet resultSet = null;
-            ArrayList<Article> articles = new ArrayList<>();
+    public static ArrayList<Article> ListerArticleType(String libelle_type) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        ArrayList<Article> articles = new ArrayList<>();
 
-            try {
-                connection = ServiceConnexionBDD.getConnection();
+        try {
+            connection = ServiceConnexionBDD.getConnection();
 
-                String query ="SELECT article.* "+
-                        "FROM article " +
-                        "JOIN type ON article.id_type = type.id " +
-                        "WHERE type.libelle = ? ";
+            String query = "SELECT article.* " +
+                    "FROM article " +
+                    "JOIN type ON article.id_type = type.id " +
+                    "WHERE type.libelle = ? ";
 
-                statement = connection.prepareStatement(query);
-                statement.setString(1,libelle_type);
-                resultSet = statement.executeQuery();
+            statement = connection.prepareStatement(query);
+            statement.setString(1, libelle_type);
+            resultSet = statement.executeQuery();
 
-                while (resultSet.next()){
-                    Article article = new Article(
-                            resultSet.getInt("id"),
-                            resultSet.getString("reference"),
-                            resultSet.getString("nom"),
-                            resultSet.getInt("id_type"),
-                            resultSet.getDouble("prix")
-                    );
-                    articles.add(article);
-                }
+            while (resultSet.next()) {
+                Article article = new Article(
+                        resultSet.getInt("id"),
+                        resultSet.getString("reference"),
+                        resultSet.getString("nom"),
+                        resultSet.getInt("id_type"),
+                        resultSet.getDouble("prix")
+                );
+                articles.add(article);
+            }
             return articles;
 
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+    }
 
     /**
      * Méthode permettant de connaître si un article existe déjà en donnant sa gamme, sa couleur, son type et sa saveur
-     * @param gamme : potentielle gamme de l'article que l'on veut rajouter
+     *
+     * @param gamme   : potentielle gamme de l'article que l'on veut rajouter
      * @param couleur : potentielle couleur de l'article que l'on veut rajouter
-     * @param type : type de l'article que l'on veut rajouter
-     * @param saveur : potentielle saveur de l'article que l'on veut rajouter
+     * @param type    : type de l'article que l'on veut rajouter
+     * @param saveur  : potentielle saveur de l'article que l'on veut rajouter
      * @return true lorsque l'article existe déjà, false lorsque aucun article de ce genre n'existe
      */
-        public static boolean getArticlePrecis(String gamme, String couleur, String type, String saveur){
-            Connection connection = null;
-            PreparedStatement statement = null;
-            ResultSet resultSet = null;
-            ArrayList<Article> articles = new ArrayList<>();
+    public static boolean getArticlePrecis(String gamme, String couleur, String type, String saveur) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        ArrayList<Article> articles = new ArrayList<>();
 
-            try {
-                connection = ServiceConnexionBDD.getConnection();
-                if (gamme == null){ //Test si le produit possède une gamme (= une gourde)
-                    if (saveur == null){ //Test si le produit possède une saveur (= une pastille) Cas pour module et accessoire
-                        String query ="SELECT article.* "+
-                                "FROM article, type, couleur " +
-                                "WHERE type.libelle = ? AND couleur.libelle = ? ";
+        try {
+            connection = ServiceConnexionBDD.getConnection();
+            if (gamme == null) { //Test si le produit possède une gamme (= une gourde)
+                if (saveur == null) { //Test si le produit possède une saveur (= une pastille) Cas pour module et accessoire
+                    String query = "SELECT article.* " +
+                            "FROM article, type, couleur " +
+                            "WHERE type.libelle = ? AND couleur.libelle = ? ";
 
-                        statement = connection.prepareStatement(query);
-                        statement.setString(1,type);
-                        statement.setString(2,couleur);
-                        resultSet = statement.executeQuery();
-                    }
-                    else{ // Cas pour les pastilles
-                        String query ="SELECT article.* "+
+                    statement = connection.prepareStatement(query);
+                    statement.setString(1, type);
+                    statement.setString(2, couleur);
+                    resultSet = statement.executeQuery();
+                } else { // Cas pour les pastilles
+                    String query = "SELECT article.* " +
                             "FROM article, couleur " +
                             "WHERE couleur.libelle = ? AND article.saveur = ?";
 
-                        statement = connection.prepareStatement(query);
-                        statement.setString(1,couleur);
-                        statement.setString(2,saveur);
-                        resultSet = statement.executeQuery();
-                    }
-                }
-                else{ //Cas pour les gourdes
-                    String query ="SELECT article.* "+
-                            "FROM article, type, couleur, gamme " +
-                            "WHERE couleur.libelle = ? AND gamme.gamme = ?";
-
                     statement = connection.prepareStatement(query);
-                    statement.setString(1,couleur);
-                    statement.setString(2,gamme);
+                    statement.setString(1, couleur);
+                    statement.setString(2, saveur);
                     resultSet = statement.executeQuery();
                 }
+            } else { //Cas pour les gourdes
+                String query = "SELECT article.* " +
+                        "FROM article, type, couleur, gamme " +
+                        "WHERE couleur.libelle = ? AND gamme.gamme = ?";
 
-                return resultSet.next(); //Renvoie true si l'article existe déjà
-
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public static String AjoutArticle(String reference, String saveur, String description, double prix, byte[] image, int id_type, int id_gamme, int id_couleur, String nom){
-            Connection connection = null;
-            PreparedStatement statement = null;
-            ResultSet resultSet = null;
-
-            try {
-                connection = ServiceConnexionBDD.getConnection();
-                String query = "INSERT INTO article(?, ?, ?, ?, ?, ?, ?, ?, ?)";
                 statement = connection.prepareStatement(query);
-                statement.setString(1,reference);
-                statement.setString(2,saveur);
-                statement.setString(3,description);
-                statement.setDouble(4,prix);
-                statement.setBytes(5, image);
-                statement.setInt(6, id_type);
-                statement.setInt(7,id_gamme);
-                statement.setInt(8,id_couleur);
-                statement.setString(9,nom);
+                statement.setString(1, couleur);
+                statement.setString(2, gamme);
                 resultSet = statement.executeQuery();
+            }
 
-                return ("Article " + reference +" rajouté");
+            return resultSet.next(); //Renvoie true si l'article existe déjà
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String AjoutArticle(String reference, String saveur, String description, double prix, byte[] image, int id_type, int id_gamme, int id_couleur, String nom) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = ServiceConnexionBDD.getConnection();
+            String query = "INSERT INTO article(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            statement = connection.prepareStatement(query);
+            statement.setString(1, reference);
+            statement.setString(2, saveur);
+            statement.setString(3, description);
+            statement.setDouble(4, prix);
+            statement.setBytes(5, image);
+            statement.setInt(6, id_type);
+            statement.setInt(7, id_gamme);
+            statement.setInt(8, id_couleur);
+            statement.setString(9, nom);
+            resultSet = statement.executeQuery();
+
+            return ("Article " + reference + " rajouté");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static ArrayList<String> ListerType() {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        ArrayList<String> listeType = new ArrayList<>();
+
+        try {
+            connection = ServiceConnexionBDD.getConnection();
+            String query = "SELECT libelle FROM type"; // Sélectionnez seulement la colonne libelle
+
+            statement = connection.prepareStatement(query);
+            resultSet = statement.executeQuery();
+
+            // Parcourir les résultats et ajouter les libellés des types à la liste
+            while (resultSet.next()) {
+                String libelleType = resultSet.getString("libelle");
+                listeType.add(libelleType);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // Fermer les ressources (ResultSet, PreparedStatement et Connection)
+            try {
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
             } catch (SQLException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
         }
+
+        return listeType;
+    }
     }
 
 
--- src/main/java/Ereverse/servlet/HomeServlet.java
+++ src/main/java/Ereverse/servlet/HomeServlet.java
@@ -13,21 +13,15 @@ import java.util.ArrayList;
 
 @WebServlet({""})
 public class HomeServlet extends HttpServlet {
-
-
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Récupérer l'identifiant de l'article à partir des paramètres de la requête
-        int id_gamme =  Integer.parseInt(request.getParameter("id_gamme"));
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            // Appeler la méthode DAO pour récupérer la liste des types d'articles depuis la base de données
+            ArrayList<String> listeTypes = ArticleDAO.ListerType();
 
-        // Utiliser cet identifiant pour charger les informations de l'article depuis la base de données
-        // (vous pouvez utiliser votre DAO pour cela)
-        ArrayList<Gourde> ListeGourde = ArticleDAO.ListerGourde(id_gamme);
+            // Ajouter la liste des types d'articles à l'attribut de la requête pour la passer à la JSP
+            request.setAttribute("ListeTypes", listeTypes);
 
-        // Mettre l'objet ListeGourde en tant qu'attribut de la requête pour l'affichage dans la JSP
-        request.setAttribute("ListeGourde", ListeGourde);
-
-        // Rediriger vers la page JSP de l'article avec les informations chargées
-        request.getRequestDispatcher("/jsp/PageArticle.jsp").forward(request, response);
+            // Rediriger vers votre page JSP
+            request.getRequestDispatcher("/jsp/home.jsp").forward(request, response);
+        }
     }
-}
